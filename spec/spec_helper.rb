8700d6b9881f8e0b39bb56b1ed17d838fada1a33
Josh Bassett <josh.bassett@gmail.com>
2017-03-02 22:21:52 +1100

    Update rspec config to verify partial doubles

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,12 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
 require "paper"
+
+RSpec.configure do |config|
+  config.mock_with :rspec do |mocks|
+    # Prevents you from mocking or stubbing a method that does not exist on a
+    # real object. This is generally recommended, and will default to `true` in
+    # RSpec 4.
+    mocks.verify_partial_doubles = true
+  end
+end
